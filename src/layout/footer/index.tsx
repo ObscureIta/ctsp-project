28499f77df032c47a3b1820a6652c8a146349836
Ivan Fedorov <Fedorov.IS@x5.ru>
2026-01-31 20:07:55 +0300

    feat: Реализован дизайн компонента footer

--- src/layout/footer/index.tsx
+++ src/layout/footer/index.tsx
@@ -1,5 +1,21 @@
+import styles from './styles.module.scss';
+
 const Footer = () => {
-  return <footer>{'This is Footer!'}</footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.signature}>
+        <span>Made by</span>
+        <a
+          href="https://github.com/ObscureIta"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.link}
+        >
+          ObscureITA
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
