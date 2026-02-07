08a202b04961a9f7baeb995f48c68dd1db02f632
Ivan Fedorov <obscure.ita@gmail.com>
2026-02-07 15:23:33 +0300

    feat: Реализация копирования через cjs скрипт + починка шрифтов

---
+++ packages/copy404.cjs
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
+const dist = path.resolve(__dirname, '../dist');
+
+fs.copyFileSync(path.join(dist, 'index.html'), path.join(dist, '404.html'));
+
+console.log('404.html создан из index.html');
